a4809e521d0e87439cf3e42377a83b86aa6e5b8f
mikekoser <mikekoser@gmail.com>
2021-04-12 20:35:41 -0400

    Added vscode snippets file with a region file. Added a login button to the home page.

--- src/pages/home/home-page.tsx
+++ src/pages/home/home-page.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import BusinessIcon from "@material-ui/icons/Business";
+import Button from "@material-ui/core/Button";
 
 const HomePage: React.FC = () => {
+    // -----------------------------------------------------------------------------------------
+    // #region Functions
+    // -----------------------------------------------------------------------------------------
+
+    const onLoginClick = () => {
+        authenticateWithGoogle();
+    };
+
+    const authenticateWithGoogle = () => {
+        // Login with google here.
+    };
+
+    // #endregion Functions
+
     // ---------------------------------------------------x--------------------------------------
     // #region Render
     // -----------------------------------------------------------------------------------------
 
     return (
-        <Typography variant="h1">
-            <BusinessIcon color="primary" />
-            Nutshell
-        </Typography>
+        <>
+            <Typography variant="h1">
+                <BusinessIcon color="primary" />
+                Nutshell
+            </Typography>
+            <div>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={onLoginClick}>
+                    Login
+                </Button>
+            </div>
+        </>
     );
 
     // #endregion Render
