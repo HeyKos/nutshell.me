033ce233e81428451918ab8b9b8a94ba2a231bdc
mikekoser <mikekoser@gmail.com>
2021-04-12 20:23:30 -0400

    Added a firebase init file to load config and initialize firebase.

---
+++ src/firebase-init.ts
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import firebaseConfig from "./firebase-config";
+import "firebase/firestore";
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    firebase.firestore().settings({ experimentalForceLongPolling: true });
+} else {
+    console.log("firebase apps already running...");
+}
+
+export default firebase;
