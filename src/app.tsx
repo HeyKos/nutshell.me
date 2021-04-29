1ec62ba3801bb334b4ed48b9b03037f02b030e2a
mikekoser <mikekoser@gmail.com>
2021-04-29 19:08:38 -0400

    Added Montserrat font, and created typography customizations to the theme.

--- src/app.tsx
+++ src/app.tsx
@@ -29,6 +29,42 @@ const App: React.FC = () => {
                 default: "#F7F9F8",
             },
         },
+        typography: {
+            fontFamily: ["Montserrat", "sans-serif"].join(","),
+            h1: {
+                color: "#8C5A40",
+                textTransform: "uppercase",
+                fontSize: "4rem",
+                fontWeight: "normal",
+            },
+            h2: {
+                fontSize: "3rem",
+                fontWeight: "bold",
+            },
+            h3: {
+                fontSize: "2rem",
+            },
+            h4: {
+                fontSize: "2rem",
+                fontWeight: "lighter",
+            },
+            h5: {
+                fontSize: "1rem",
+            },
+            h6: {
+                fontSize: "1rem",
+                fontWeight: "lighter",
+            },
+            button: {
+                fontWeight: "normal",
+            },
+            body1: {
+                fontWeight: "normal",
+            },
+            body2: {
+                fontWeight: "lighter",
+            },
+        },
     });
 
     // #endregion Theme
