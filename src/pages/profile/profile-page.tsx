e5ecb72f35f221fa754ae722439714ff7697790c
mikekoser <mikekoser@gmail.com>
2021-04-29 20:32:31 -0400

    Scaffolded a profile page.

---
+++ src/pages/profile/profile-page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const ProfilePage: React.FC = () => {
+    // ---------------------------------------------------x--------------------------------------
+    // #region Render
+    // -----------------------------------------------------------------------------------------
+
+    return <Typography variant="h1">Profile</Typography>;
+
+    // #endregion Render
+};
+
+export default ProfilePage;
