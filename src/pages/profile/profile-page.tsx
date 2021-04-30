8fd6cdfc687aed161af8f213047342c3be5305aa
mikekoser <mikekoser@gmail.com>
2021-04-29 20:37:15 -0400

    Added connect strava button to profile page.

--- src/pages/profile/profile-page.tsx
+++ src/pages/profile/profile-page.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
 const ProfilePage: React.FC = () => {
+    const onStravaClick = () => {
+        console.log("Strava clicked");
+    };
     // ---------------------------------------------------x--------------------------------------
     // #region Render
     // -----------------------------------------------------------------------------------------
 
-    return <Typography variant="h1">Profile</Typography>;
+    return (
+        <div>
+            <Typography variant="h1">Profile</Typography>
+            <Button variant="contained" color="default" onClick={onStravaClick}>
+                Connect Strava
+            </Button>
+        </div>
+    );
 
     // #endregion Render
 };
